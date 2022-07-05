9a3849eb6f510c29c836275b07bc6a2e39148a9c
Caroline Chen <caroline@Carolines-iMac.local>
2022-07-05 10:28:26 -0400

    store income amount and source in local storage

--- cache-app/frontend/components/App.js
+++ cache-app/frontend/components/App.js
@@ -1,44 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import AddIncomeForm from "./AddIncomeForm";
 import Accounts from "./Accounts";
 
-export default class App extends React.Component {
-	constructor(window) {
-		super();
-		this.state = {
-			amount: 0,
-			source: ""
-		}
-	}
+export default function App() {
+	const [income, setIncome] = useState(0);
+	const [source, setSource] = useState("");
 
-	addIncome = income => {
-		//take a copy of the existing state
-		const incomeState = {...this.state}
-		//update the state
-		incomeState.amount = income.amount;
-		incomeState.source = income.source;
-		//set the new object to state
-		this.setState({
-			amount: incomeState.amount,
-			source: incomeState.source
-		})
-	};
-	updateIncome = (depositAmount) => {
-		const incomeState = {...this.state}
-		incomeState.amount -= depositAmount;
-		this.setState({
-			amount: incomeState.amount
-		})
+	useEffect(() => {
+    	setIncome(JSON.parse(window.localStorage.getItem('income')));
+    	setSource(window.localStorage.getItem('source'));
+  	}, []);
+
+
+
+  	useEffect(() => {
+    	window.localStorage.setItem('income', income);
+    	window.localStorage.setItem('source', source);
+  	}, [income, source]);
+
+
+	const addIncome = income => {
+		return (
+			setIncome(income.amount),
+			setSource(income.source)
+		)
 	};
 
-	render() {
+	const updateIncome = depositAmount => {
+		let newIncome = income;
+		newIncome -= depositAmount;
+		return setIncome(newIncome)
+	};
+
+
 		return (
 			<div>
-				<div className="income">Income: ${this.state.amount}</div>
-				<div className="income">Source: {this.state.source}</div>
-				<AddIncomeForm addIncome={this.addIncome} />
-				<Accounts updateIncome={this.updateIncome} incomeSource={this.state.source}/>
+				<div className="income">Income: ${income}</div>
+				<div className="income">Source: {source}</div>
+				<AddIncomeForm addIncome={addIncome} />
+				<Accounts updateIncome={updateIncome} incomeSource={source}/>
 			</div>
 		)
-	}
+	
 }
